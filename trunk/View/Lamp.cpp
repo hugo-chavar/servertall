0ee6737faa354492c71efbe0ba0cce24ce0e17b3
hechavar <hechavar@056bb513-ba45-4432-0627-e7cb486d07af>
2013-06-08 22:51:49 +0000

    Item Lámpara Ok!

--- trunk/View/Lamp.cpp
+++ trunk/View/Lamp.cpp
@@ -1,5 +1,5 @@
 #include "Lamp.h"
-#define FACTORVISION 1.5 //factor de aumento del rango de vision
+//#define FACTORVISION 1.5 //factor de aumento del rango de vision
 
 Lamp::Lamp(string _name,string _hidden,std::pair <int,int> _pos,Sprite* _hiddenSprite, Sprite* sprite) : ItemView(_name,_hidden,_pos,_hiddenSprite,sprite)
 {
@@ -12,7 +12,6 @@ Lamp::~Lamp(void)
 
 void Lamp::modifyCharacter(Personaje* personaje)
 {
-	//TODO:Implement 
-	
-	//personaje->increaseVisionRange(FACTORVISION);
+	int incrementar = 3; //TODO: Yami, traelo del parser
+	personaje->personajeModelo()->getVision()->increaseRangeVision(incrementar);
 }

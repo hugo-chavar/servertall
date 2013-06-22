df347607539447328c1ae5a587533781a6facef8
hechavar <hechavar@056bb513-ba45-4432-0627-e7cb486d07af>
2013-06-22 20:37:05 +0000

    Evito que se bloquee al disparar un encanto

--- View/IceIncantation.cpp
+++ View/IceIncantation.cpp
@@ -13,6 +13,8 @@ IceIncantation::~IceIncantation() {
 
 void IceIncantation::impact(Daniable* daniable) {
 	if (daniable) {
+		if (daniable->getPosition() == this->getInitialTile())
+			return;
 		GameView::instance().addEventUpdate(stringUtilities::intToString(EVENT_SOUND_ICESPELL_IMPACT)+";"+stringUtilities::pairIntToString(this->getCurrentTile()));
 		daniable->iceUp(5);
 		this->setTargetReached(true);

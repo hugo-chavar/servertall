8ea8550e549443c7559140472717c4d655503a02
Imperoris@gmail.com <Imperoris@gmail.com@056bb513-ba45-4432-0627-e7cb486d07af>
2013-06-23 03:04:20 +0000

    Modificación en el reset del pj.

--- trunk/View/Personaje.cpp
+++ trunk/View/Personaje.cpp
@@ -313,7 +313,15 @@ void Personaje::calcularSigTileAMover(){
 }
 
 void Personaje::reset() {
+	this->setPosition(modelo->getPosition());
 	this->setRectangle(this->getPosition(), sprites[this->currentSpritePosition]);
+	this->setCurrentSpritePosition(this->calculateSpritePosition(modelo->getEstado()));
+	velocidad = modelo->getVelocidad();
+	delta.first = 0;
+	delta.second = 0;
+	ePot.first = 0;
+	ePot.second = 0;
+	serr = 0;
 	currentEnemy = NULL;
 	this->heal();
 	this->rechargeMagic();
@@ -327,6 +335,9 @@ void Personaje::reset() {
 	vidCost = 0;
 	vidTime = 0;
 	videncia = false;
+	this->setIceSpell(false);
+	this->setFogged(false);
+	this->resetSpriteState();
 	modelo->setIsReseting();
 }
 
